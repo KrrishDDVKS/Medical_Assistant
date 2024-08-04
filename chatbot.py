da8d50a2d8c0a2edd4c8041e14f2d199b1a34cc6
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2024-08-03 20:08:41 -0500

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import load_dotenv,dotenv_values
+from langchain_pinecone import PineconeVectorStore
+from langchain_openai import OpenAIEmbeddings
+from langchain_core.vectorstores import VectorStoreRetriever
+from langchain.chains import RetrievalQA
+from langchain_openai import ChatOpenAI
+
+load_dotenv("/keys.env")
+
+config = dotenv_values("keys.env")
+os.environ['OPENAI_API_KEY'] = config['OPEN_API_KEY']
+os.environ['PINECONE_API_KEY'] = config['PINE_CONE_KEY']
+
+
+index_name = "disease-symptoms-gpt-4"
+
+embed = OpenAIEmbeddings(
+model='text-embedding-ada-002',
+openai_api_key=os.environ.get('OPEN_API_KEY')
+)
+
+
+vectorstore = PineconeVectorStore(index_name=index_name, embedding=embed)
+
+
+
+retriever = VectorStoreRetriever(vectorstore=vectorstore)
+qa_chain = RetrievalQA.from_chain_type(
+llm=ChatOpenAI(api_key=os.environ.get("OPEN_API_KEY"),model_name='gpt-4o',
+temperature=0.0),
+chain_type="stuff",
+retriever=retriever
+)
+
+import streamlit as st
+st.title("💬 Chatbot")
+if "messages" not in st.session_state:
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input():
+    
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+    answer = qa_chain.run(query=prompt)
+    st.session_state.messages.append({"role": "assistant", "content": answer})
+    st.chat_message("assistant").write(answer)
