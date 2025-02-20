9827cc5d1fda9d1668571eb8e86fe1a0d8a40762
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2025-02-19 23:54:28 -0600

    Update Symptom_Checker.py

--- Symptom_Checker.py
+++ Symptom_Checker.py
@@ -39,20 +39,6 @@ PROMPT = PromptTemplate(
     template=prompt_template, input_variables=["context"]
 )
 
-chain = LLMChain(llm=llm, prompt=prompt_template)
-if chain=='Yes':
-    prompt_template='''Accept the user’s symptoms as input and provide probable diseases, diagnoses and prescription using only the information stored in the vector database. politely inform the user that the data is insufficient to provide a diagnosis when the given prompt is not relavent to Medical Symptoms.    
-    Text:
-    {context}'''
-PROMPT = PromptTemplate(
-    template=prompt_template, input_variables=["context"]
-)
-
-retriever = VectorStoreRetriever(vectorstore=vectorstore)
-qa_chain = RetrievalQA.from_chain_type(llm=llm,
-        chain_type="stuff",
-        retriever=retriever,
-        chain_type_kwargs={"prompt": PROMPT},)
 st.markdown("<h1 style='text-align: center; color: black;'>MediConnect AI 🏥</h1>", unsafe_allow_html=True)
 st.header("Symptom-Based Diagnosis :mask:")
 st.write(
@@ -73,6 +59,21 @@ if prompt := st.chat_input():
     
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
+    chain.run(prompt) = LLMChain(llm=llm, prompt=prompt_template)
+    if chain=='Yes':
+        prompt_template='''Accept the user’s symptoms as input and provide probable diseases, diagnoses and prescription using only the information stored in the vector database. politely inform the user that the data is insufficient to provide a diagnosis when the given prompt is not relavent to Medical Symptoms.    
+        Text:
+        {context}'''
+    PROMPT = PromptTemplate(
+        template=prompt_template, input_variables=["context"]
+    )
+
+retriever = VectorStoreRetriever(vectorstore=vectorstore)
+qa_chain = RetrievalQA.from_chain_type(llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        chain_type_kwargs={"prompt": PROMPT},)
+
     answer = qa_chain.run(query=prompt)
     st.session_state.messages.append({"role": "assistant", "content": answer})
     st.chat_message("assistant").write(answer)
