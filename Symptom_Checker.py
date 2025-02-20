ff83a0d30f3c53f3541edfe79a3fb9ed5fd90696
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2025-02-19 23:48:30 -0600

    Update Symptom_Checker.py

--- Symptom_Checker.py
+++ Symptom_Checker.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv,dotenv_values
 from langchain_pinecone import PineconeVectorStore
 from langchain_openai import OpenAIEmbeddings
 from langchain_core.vectorstores import VectorStoreRetriever
-from langchain.chains import RetrievalQA,LLMChain
+from langchain.chains import RetrievalQA
+from langchain.chains import LLMChain
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 import streamlit as st
