31f08d90420aaaf2a979ff72e6bcea4741da9141
Desaboina Krishna Sai <42516247+KrrishDDVKS@users.noreply.github.com>
2026-02-11 19:56:40 -0600

    Refactor Symptom_Checker.py for improved clarity

--- Symptom_Checker.py
+++ Symptom_Checker.py
@@ -76,9 +76,10 @@ if prompt1 := st.chat_input():
         st.write(texts)
         
         
-        prompt='''Accept the user’s symptoms as input and provide as output the probable diseases, diagnoses and prescription using only the information stored in the vector database. politely inform the user that the data is insufficient to provide a diagnosis when the given prompt is not relavent to Medical Symptoms.    
+        prompt='''    
         Symptoms:
         {texts}
+        Accept the user’s symptoms as input and provide as output the probable diseases, diagnoses and prescription using only the information stored in the vector database.
         Disease:'''
 
         response = client.chat.completions.create(
@@ -101,6 +102,7 @@ if prompt1 := st.chat_input():
 
         st.chat_message("assistant").write(answer)
         st.chat_message("assistant").write("This is answered by second Agent. The Main purpose of this app is to detect disease from symptom. Please provide the Symptom")
+
 
 
 
